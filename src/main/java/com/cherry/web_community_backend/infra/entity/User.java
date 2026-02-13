d7f516a9e354acb54c5a16c94a61bab42c26fc16
cherryblossom0011 <cherrybls0dev@gmail.com>
2026-02-14 04:33:08 +0900

    feat: Create Entity --- 엔티티 생성 sql 연결 안함

---
+++ src/main/java/com/cherry/web_community_backend/infra/entity/User.java
@@ -0,0 +1,30 @@
+package com.cherry.web_community_backend.infra.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 36)
+	private String uuid;
+
+	@Column(length = 50)
+	private String nickname;
+
+	@Column(name = "login_id", length = 50)
+	private String loginId;
+
+	@Column(name = "user_pw", length = 255)
+	private String userPW;
+
+}
