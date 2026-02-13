f7b41a151ec20a4ebce2b0ea0bfd59a9a1dfa70f
cherryblossom0011 <cherrybls0dev@gmail.com>
2026-02-14 00:48:09 +0900

    init: Create web-community-backend Project --- 스프링 백엔드 개발 프로젝트 생성

---
+++ src/test/java/com/cherry/web_community_backend/WebCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.web_community_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommunityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
